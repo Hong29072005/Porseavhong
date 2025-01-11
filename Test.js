11a7e061c68ce85f530792e7114a9418d54c773a
Hong29072005 <mrhong563@gmail.com>
2025-01-11 09:59:21 +0700

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,12 @@
+
+function menu_bar(){
+   document.getElementById('ul_bar').style.display='block'
+   
+}
+function menu_bars(){
+    document.getElementById('ul_bar').style.display='none'
+    
+ }
+// function iconbar(){
+//    document.getElementById('icon_bar').style.display='none'
+// }
